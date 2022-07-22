f7ca68b07d5c839a420b823c6bf4f7e24a33c956
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2022-07-22 12:40:11 -0300

    First commit

--- lib/first/gem/mayaugusto.rb
+++ lib/first/gem/mayaugusto.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-require_relative "mayaugusto/version"
+require_relative 'mayaugusto/version'
+require_relative 'mayaugusto/translator'
 
 module First
   module Gem
     module Mayaugusto
       class Error < StandardError; end
-      # Your code goes here...
+      def self.hi(language = "english")
+        translator = First::Gem::Mayaugusto::Translator.new(language)
+        translator.hi
+      end
     end
   end
 end
--- lib/first/gem/mayaugusto/translator.rb
+++ lib/first/gem/mayaugusto/translator.rb
@@ -1,2 +1,23 @@
-class Translator
+module First
+  module Gem
+    module Mayaugusto
+      class Translator
+        def initialize(language)
+          @language = language
+        end
+
+        def hi
+          case @language
+          when 'spanish'
+            'hola mundo!'
+          when 'portuguese'
+            'olá mundo!'
+          else
+            'hello world!'
+          end
+        end
+      end
+    end
+  end
 end
+
